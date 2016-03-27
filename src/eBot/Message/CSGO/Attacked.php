406948fc803cd489cd6492c191159b47640cf2f8
Håvard Brøseth Nordli <wow.howard_94@hotmail.com>
2016-03-27 04:19:03 +0200

    Fix regex for catching Attacked messages.

--- src/eBot/Message/CSGO/Attacked.php
+++ src/eBot/Message/CSGO/Attacked.php
@@ -5,7 +5,7 @@ use eBot\Message\Message;
 use eBot\Message\Type\Attacked as Object;
 class Attacked extends Message {
     public function __construct() {
-        parent::__construct('/^"(?P<attackerName>.+)[<](?P<attackerUserId>\d+)[>][<](?P<attackerSteamId>.*)[>][<](?P<attackerTeam>CT|TERRORIST|Unassigned|Spectator)[>])" \[(?P<attackerPosX>[\-]?[0-9]+) (?P<attackerPosY>[\-]?[0-9]+) (?P<attackerPosZ>[\-]?[0-9]+)\] attacked "(?P<victimName>.+)[<](?P<victimUserId>\d+)[>][<](?P<victimSteamId>.*)[>][<](?P<victimTeam>CT|TERRORIST|Unassigned|Spectator)[>]" \[(?P<victimPosX>[\-]?[0-9]+) (?P<victimPosY>[\-]?[0-9]+) (?P<victimPosZ>[\-]?[0-9]+)\] with "(?P<attackerWeapon>[a-zA-Z0-9_]+)" \(damage "(?P<attackerDamage>[0-9]+)"\) \(damage_armor "(?P<attackerDamageArmor>[0-9]+)"\) \(health "(?P<victimHealth>[0-9]+)"\) \(armor "(?P<victimArmor>[0-9]+)"\) \(hitgroup "(?P<attackerHitGroup>.*)"\)/');
+        parent::__construct('/^"(?P<attackerName>.*)[<](?P<attackerUserId>\d+)[>][<](?P<attackerSteamId>.*)[>][<](?P<attackerTeam>CT|TERRORIST|Unassigned|Spectator)[>]" \[(?P<attackerPosX>[\-]?[0-9]+) (?P<attackerPosY>[\-]?[0-9]+) (?P<attackerPosZ>[\-]?[0-9]+)\] attacked "(?P<victimName>.*)[<](?P<victimUserId>\d+)[>][<](?P<victimSteamId>.*)[>][<](?P<victimTeam>CT|TERRORIST|Unassigned|Spectator)[>]" \[(?P<victimPosX>[\-]?[0-9]+) (?P<victimPosY>[\-]?[0-9]+) (?P<victimPosZ>[\-]?[0-9]+)\] with "(?P<attackerWeapon>[a-zA-Z0-9_]+)" \(damage "(?P<attackerDamage>[0-9]+)"\) \(damage_armor "(?P<attackerDamageArmor>[0-9]+)"\) \(health "(?P<victimHealth>[0-9]+)"\) \(armor "(?P<victimArmor>[0-9]+)"\) \(hitgroup "(?P<attackerHitGroup>.*)"\)/');
     }
 
     public function process() {
